ebde2b0e5eb3139239fb8b2f5b1f23495feba21a
Yeasin <jabed50000@gmail.com>
2025-07-10 15:46:58 +0600

    feat: Add Copilot Chat plugin

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,31 @@
+return {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "main", -- or "main"
+    dependencies = {
+        "zbirenbaum/copilot.lua", -- already in your setup
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        show_help = true,
+        auto_follow_cursor = true,
+        debug = false,
+    },
+    keys = {
+        {
+            "<leader>cc",
+            function()
+                require("CopilotChat").ask("Fix this code")
+            end,
+            mode = "v",
+            desc = "Copilot Chat: Fix selected code",
+        },
+        {
+            "<leader>cq",
+            function()
+                require("CopilotChat").ask()
+            end,
+            mode = "v",
+            desc = "Copilot Chat: Ask Copilot (selected code)",
+        },
+    },
+}
