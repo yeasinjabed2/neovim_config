baf9f26a00bf4c18dc28baabcca7a7d779b79809
Yeasin Jabed <jabed50000@gmail.com>
2026-05-10 21:18:02 +0600

    fix: add dart formatter to conform config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,89 +1,95 @@
 local conform = require("conform")
 
 conform.setup({
-    formatters_by_ft = {
-        -- Web
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
-        javascriptreact = { "prettier" },
-        json = { "prettier" },
-        jsonc = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        html = { "prettier" },
-        css = { "prettier" },
-        scss = { "prettier" },
-        less = { "prettier" },
-        graphql = { "prettier" },
-        -- Python
-        python = { "black", "isort" },
-        -- Rust
-        rust = { "rustfmt" },
-        -- C/C++
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-        -- Go
-        go = { "gofmt" },
-        -- Java
-        java = { "google-java-format" },
-        -- Kotlin
-        kotlin = { "ktlint" },
-        -- C#
-        cs = { "csharpier" },
-        -- PHP
-        php = { "php-cs-fixer" },
-        -- Ruby
-        ruby = { "rubocop" },
-        -- Lua
-        lua = { "stylua" },
-        -- Shell
-        sh = { "shfmt" },
-        bash = { "shfmt" },
-        -- SQL
-        sql = { "sqlfluff" },
-        -- Dockerfile
-        dockerfile = { "hadolint" },
-        -- TOML
-        toml = { "taplo" },
-        -- XML
-        xml = { "xmllint" },
+  opts = {
+    format_on_save = true,
+  },
+
+  formatters_by_ft = {
+    -- Web
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascriptreact = { "prettier" },
+    json = { "prettier" },
+    jsonc = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    html = { "prettier" },
+    css = { "prettier" },
+    scss = { "prettier" },
+    less = { "prettier" },
+    graphql = { "prettier" },
+    -- Python
+    python = { "black", "isort" },
+    -- Rust
+    rust = { "rustfmt" },
+    -- C/C++
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+    -- Go
+    go = { "gofmt" },
+    -- Java
+    java = { "google-java-format" },
+    -- Kotlin
+    kotlin = { "ktlint" },
+    -- C#
+    cs = { "csharpier" },
+    -- PHP
+    php = { "php-cs-fixer" },
+    -- Ruby
+    ruby = { "rubocop" },
+    -- Lua
+    lua = { "stylua" },
+    -- Shell
+    sh = { "shfmt" },
+    bash = { "shfmt" },
+    -- SQL
+    sql = { "sqlfluff" },
+    -- Dockerfile
+    dockerfile = { "hadolint" },
+    -- TOML
+    toml = { "taplo" },
+    -- XML
+    xml = { "xmllint" },
+    -- flutter
+    dart = { "dart_format" },
+  },
+  formatters = {
+    prettier = {
+      stdin = true,
     },
-    formatters = {
-        prettier = {
-            stdin = true,
-        },
-        black = {
-            prepend_args = { "--line-length=88" },
-        },
-        isort = {
-            prepend_args = { "--profile", "black" },
-        },
-        rustfmt = {
-            prepend_args = { "--edition", "2021" },
-        },
-        clang_format = {
-            prepend_args = { "--style=file" },
-        },
-        gofmt = {},
-        shfmt = {
-            prepend_args = { "-i", "2" },
-        },
-        sqlfluff = {
-            prepend_args = { "format", "--dialect", "postgres" },
-        },
-        stylua = {
-            prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
-        },
+    black = {
+      prepend_args = { "--line-length=88" },
     },
+    isort = {
+      prepend_args = { "--profile", "black" },
+    },
+    rustfmt = {
+      prepend_args = { "--edition", "2021" },
+    },
+    clang_format = {
+      prepend_args = { "--style=file" },
+    },
+    gofmt = {},
+    shfmt = {
+      prepend_args = { "-i", "2" },
+    },
+    sqlfluff = {
+      prepend_args = { "format", "--dialect", "postgres" },
+    },
+    stylua = {
+      prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+    },
+  },
 })
 
 -- Auto format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        conform.format({ bufnr = args.buf, lsp_fallback = true })
-    end,
+  pattern = "*",
+  callback = function(args)
+    conform.format({ bufnr = args.buf, lsp_fallback = true })
+  end,
 })
 
 -- ~/.config/prettier/.prettierrc.json
@@ -103,7 +109,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 --   "proseWrap": "preserve",
 --   "endOfLine": "lf"
 -- }
-
 
 -- # Web
 -- npm install -g prettier
